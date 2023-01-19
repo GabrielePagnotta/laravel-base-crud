2ee77ff6b0ba8971360eddf3bcde69b47d4382cf
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2023-01-19 23:21:29 +0100

    end

--- app/Http/Controllers/ComixController.php
+++ app/Http/Controllers/ComixController.php
@@ -14,7 +14,7 @@ class ComixController extends Controller
      */
     public function index()
     {
-        $comixes=comix::All();
+        $comixes=comix::paginate(10);
 
 
         return view("comix.index", compact('comixes'));
@@ -27,7 +27,7 @@ class ComixController extends Controller
      */
     public function create()
     {
-        //
+       return view('comix.create');
     }
 
     /**
@@ -38,7 +38,17 @@ class ComixController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $holding=$request->all();
+
+        $new_request=new comix();
+        $new_request->title=$holding["title"];
+        $new_request->description=$holding["description"];
+        $new_request->price=$holding["price"];
+        $new_request->date=$holding["sale_date"];
+        $new_request->type=$holding["type"];
+        $new_request->save();
+
+        return redirect()->route('comix.index');
     }
 
     /**
@@ -49,7 +59,9 @@ class ComixController extends Controller
      */
     public function show($id)
     {
-        //
+        $array_comix=comix::findorFail($id);
+
+        return view('comix.show',compact('array_comix'));
     }
 
     /**
